96f41d805af391ce34ad714286a4823cc84a85a9
Cristian Buffa <bufface@gmail.com>
2017-06-03 11:12:39 -0300

    71. Creating User Records

--- 6.Server Setup - Authentication/server/controllers/authentication.js
+++ 6.Server Setup - Authentication/server/controllers/authentication.js
@@ -5,10 +5,25 @@ exports.signup = function (req, res, next) {
   const password = req.body.password;
 
   // See if a user with the given email exists
+  User.findOne({ email: email }, function (err, existingUser) {
+    if (err) { return next(err); }
 
-  // If a user with email does exist, return an error
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
 
-  // If a user with email does NOT exist, create and save user record
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
 
-  // Respond to request  indicating the user was created;
+    user.save(function (err) {
+      if (err) { return next(err); }
+
+      // Respond to request  indicating the user was created
+      res.json({ success: true });
+    });
+  });
 };
