3c7ace280ebf788ddf8b07cb272f5d37b34daf80
Cristian Buffa <bufface@gmail.com>
2017-06-04 20:33:58 -0300

    79. Making an Authenticated Request

--- 6.Server Setup - Authentication/server/services/passport.js
+++ 6.Server Setup - Authentication/server/services/passport.js
@@ -1,15 +1,14 @@
 const passport = require('passport');
 const User = require('../models/user');
-const config = require('../confg');
+const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // Setup options for JWT Strategy
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secret: config.secret
+  secretOrKey: config.secret
 };
-
 // Create a JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
   // see if the user ID in the payload exist in our database
