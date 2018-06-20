885ec49ce1cb4cdc9a6bf6392f338ea1e97483f7
Cristian Buffa <bufface@gmail.com>
2018-06-20 18:43:12 -0300

    32. Providing Fake Events

--- 2.Testing/src/components/__tests__/CommentBox.test.js
+++ 2.Testing/src/components/__tests__/CommentBox.test.js
@@ -17,3 +17,9 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a textarea that users can type in', () => {
+  expect(wrapped.find('textarea')).simulate('change', {
+    target: { value: 'new comment' }
+  });
+});
