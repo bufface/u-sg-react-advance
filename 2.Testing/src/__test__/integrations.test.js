fe6c899d61dce5bfbe5c80661858174710452558
Cristian Buffa <bufface@gmail.com>
2018-07-22 22:06:51 -0300

    56. Fixing a Broken Test

--- 2.Testing/src/__test__/integrations.test.js
+++ 2.Testing/src/__test__/integrations.test.js
@@ -5,6 +5,11 @@ import App from 'components/App';
 
 it('can fetch a list of comments and display them', () => {
   // Attemp to render the *entire* app
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
   // find the 'fetchComments' buttons and click it
   // Expect to find a list of comments!
 });
